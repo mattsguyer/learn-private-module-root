9bde9ac6b2f013fb32e88445b95cdd5ff77590e1
mattsguyer <107811257+mattsguyer@users.noreply.github.com>
2025-01-28 21:44:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,9 @@ module "s3-webapp" {
   prefix  = var.prefix
   version = "1.0.0"
 }
+
+module "s3-web-app" {
+  source  = "app.terraform.io/Training-matt/s3-web-app/asw"
+  version = "1.0.0"
+  # insert required variables here
+}

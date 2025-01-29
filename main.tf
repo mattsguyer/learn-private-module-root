fbd3ffab40477f383dca830a21c8a86d6f63995d
mattsguyer <107811257+mattsguyer@users.noreply.github.com>
2025-01-28 21:51:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,5 +5,7 @@ provider "aws" {
 module "s3-web-app" {
   source  = "app.terraform.io/Training-matt/s3-web-app/asw"
   version = "1.0.0"
-  # insert required variables here
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
 }

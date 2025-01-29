a97e622f8853ea83c31ba3ea7399acc2e97b6711
mattsguyer <107811257+mattsguyer@users.noreply.github.com>
2025-01-28 21:49:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name    = var.name
-  region  = var.region
-  prefix  = var.prefix
-  version = "1.0.0"
-}
-
 module "s3-web-app" {
   source  = "app.terraform.io/Training-matt/s3-web-app/asw"
   version = "1.0.0"
